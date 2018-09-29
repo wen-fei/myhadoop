2ee28b59d94ef9a1f30ed523e0d063fac96ba879
wen-fei <282699766@qq.com>
2018-09-29 20:53:38 +0800

    Partitioner

---
+++ MR/src/main/java/org/wds/mr/PartitionerWordCount.java
@@ -0,0 +1,130 @@
+package org.wds.mr;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Partitioner;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
+/**
+ * 使用MapReduce开发wordcount应用程序
+ */
+public class PartitionerWordCount {
+
+
+    /**
+     * map: 读取输入的文件
+     */
+    public static class MyMapper extends Mapper<LongWritable, Text, Text, LongWritable> {
+
+
+        @Override
+        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            // 接收到的每一行数据
+            String line = value.toString();
+
+            // 按照分隔符拆分
+            String[] words = line.split(" ");
+            context.write(new Text(words[0]), new LongWritable(Long.parseLong(words[1])));
+        }
+    }
+
+
+    /**
+     * Reduce:归并操作
+     */
+    public static class MyReducer extends Reducer<Text, LongWritable, Text, LongWritable> {
+        @Override
+        protected void reduce(Text key, Iterable<LongWritable> values, Context context) throws IOException, InterruptedException {
+
+            long sum = 0;
+
+            for (LongWritable value : values) {
+                // 求key出现的次数总和
+                sum += value.get();
+            }
+            // 最终统计结果的输出
+            context.write(key, new LongWritable(sum));
+
+        }
+    }
+
+
+    public static class MyPartitioner extends Partitioner<Text, LongWritable> {
+        @Override
+        public int getPartition(Text key, LongWritable value, int numPartitions) {
+            if (key.toString().equals("xiaomi")) {
+                return 0;
+            }
+
+            if (key.toString().equals("huawei")) {
+                return 1;
+            }
+            if (key.toString().equals("iphone7")) {
+                return 2;
+            }
+            return 3;
+        }
+    }
+
+    /**
+     * 定义Driver : 封装了MapReduce作业的所有信息
+     * @param args
+     */
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+
+        // 设置
+        Configuration conf = new Configuration();
+
+        // 清理已存在的输出目录
+        Path outpath = new Path(args[1]);
+        FileSystem fileSystem = FileSystem.get(conf);
+        if (fileSystem.exists(outpath)) {
+            fileSystem.delete(outpath, true);
+            System.out.println("output file exists, but it has deleted");
+        }
+
+
+        // 创建job
+        Job job = Job.getInstance(conf, "wordcount");
+
+        // 设置job的处理类
+        job.setJarByClass(PartitionerWordCount.class);
+
+        // 设置作业处理的输入路径
+        FileInputFormat.setInputPaths(job, new Path(args[0]));
+
+        // 设置Map相关参数
+        job.setMapperClass(MyMapper.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(LongWritable.class);
+
+        // 设置reduce相关参数
+        job.setReducerClass(MyReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(LongWritable.class);
+
+
+        // 设置job的partition
+        job.setPartitionerClass(MyPartitioner.class);
+        // 设置4个reducer，每个分区一个
+        job.setNumReduceTasks(4);
+
+        // 设置作业处理的输出路径
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        // 退出
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+
+    }
+
+
+}
